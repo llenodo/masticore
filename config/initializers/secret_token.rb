54d3e87deca0fadfb4a89c167a166a39e2caaf16
Ryan O'Donnell <ryanjodonnell@gmail.com>
2012-09-08 14:08:21 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masticore::Application.config.secret_token = '68f48acc7c2ea0b5b8bf2b7bf1ab53a244e583459e029c0a83cbac7a8a642ab764def81f3609b52f7e4d9848418ef768ae7c1366653da2e2d01f2d0bc0eb77f6'
